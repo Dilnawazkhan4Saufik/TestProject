63e2d1fc078d7d29bfadc124fff7ef0aa82b71b1
DilnawazKhan <dilnawazkhan4saufik@gmail.com>
2022-02-09 13:32:17 +0500

    hello world text

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello world!</Text>
+    <View style={styles.container}>
+      <Text style={styles.sampleText}>Hello World!</Text>
     </View>
   );
 };
-
 export default App;
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sampleText: {
+    fontSize: 28,
+    color: 'dodgerblue',
+    fontWeight: 'bold',
+  },
+});
